dfd45e009fa8ef17998a2c53bd1f0da9c9adda32
DesarrolladoresXX <NPicci19@gmail.com>
2022-04-19 12:09:45 -0300

    cambios guardaos

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 const items = document.getElementById('items')
 const templateCard = document.getElementById('template-card').content
 const fragment = document.createDocumentFragment()
+let carrito = {}
 
 document.addEventListener('DOMContentLoaded', () => {
     fetchData()
+})
+items.addEventListener('click', e => {
+    addCarrito(e)
 })
 
 
@@ -29,4 +33,28 @@ const card = data => {
         fragment.appendChild(clone)
     })
     items.appendChild(fragment)
+}
+
+// agregar carrito
+
+const addCarrito = e => {
+    if (e.target.classList.contains('btn-dark')) {
+        setCarrito(e.target.parentElement)
+    }
+    e.stopPropagation()
+}
+
+const setCarrito = objeto => {
+    const producto = {
+        id: objeto.querySelector('.btn-dark').dataset.id,
+        nombre: objeto.querySelector('h5').textContent,
+        precio: objeto.querySelector('p').textContent,
+        cantidad: 1
+    }
+    if(carrito.hasOwnProperty(producto.id)) {
+        producto.cantidad = carrito[producto.id].cantidad +1
+    }
+
+    carrito[producto.id] = {...producto}
+    console.log(carrito)
 }
